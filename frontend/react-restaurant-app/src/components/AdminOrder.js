1e75f451868e5b55de197f06f5b42de1b50edb21
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-06-15 17:14:09 -0400

    editing order from admin orders page

--- frontend/react-restaurant-app/src/components/AdminOrder.js
+++ frontend/react-restaurant-app/src/components/AdminOrder.js
@@ -1,6 +1,41 @@
 import { useState } from 'react'
+import Cookies from 'js-cookie';
 
-const AdminOrder = ({data, completed, cancelled}) => {
+function handleError(err) {
+	console.warn(err);
+}
+
+const AdminOrder = ({id, data, completed, cancelled}) => {
+    const [state, setState] = useState({
+        id: id,
+        data: data,
+        completed: completed,
+        cancelled: cancelled,
+    })
+    
+    const updateOrder = async (newState) => {
+
+		const options = {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+				'X-CSRFToken': Cookies.get('csrftoken'),
+			},
+			body: JSON.stringify(newState),
+		}
+
+		const response = await fetch(`/api_v1/orders/${state.id}/`, options).catch(handleError);
+		
+		if(!response.ok) {
+			throw new Error('Network response was not ok!');
+		}
+
+        const data = await response.json();
+        console.log(data);
+        setState(data);
+
+	}
+    
     const items = data.items;
     let table;
 
@@ -33,6 +68,10 @@ const AdminOrder = ({data, completed, cancelled}) => {
             <div className="left">
                 <p>{data.customerName}</p>
                 {table}
+                <label htmlFor="completed">Completed</label>
+                <input type="checkbox" id="completed" defaultChecked={state.completed} onChange={(e) => updateOrder({...state, completed: e.target.checked})}/>
+                <label htmlFor="cancelled">Cancelled</label>
+                <input type="checkbox" id="cancelled" defaultChecked={state.cancelled} onChange={(e) => updateOrder({...state, cancelled: e.target.checked})}/> 
             </div>
         </article>
     )
